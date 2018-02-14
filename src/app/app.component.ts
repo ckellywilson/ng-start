9475c7c60a9347b9fb838f833bd82ac2b8d26990
Kelly Wilson <kelly.wilson@varidesk.com>
2018-02-14 15:46:54 -0600

    ng-start initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+  <h1>{{getTitle()}}</h1>
+  <app-products></app-products>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Acme Product Management';
+  getTitle(): string {
+    return this.pageTitle;
+  }
 }
