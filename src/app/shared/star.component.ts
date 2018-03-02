6f048004cdb3bc6f73c86908098110d262c72f19
ckellywilson <christopherkwilson@sbcglobal.net>
2018-03-02 00:38:16 -0600

    Add star component with Input/Output decorators

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
-  selector: 'app-star',
+  // tslint:disable-next-line:component-selector
+  selector: 'pm-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges {
   starWidth: number;
-  rating: number;
+  @Input() rating = 0;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  public onClick(): void {
+    this.ratingClicked.emit(`The rating of ${this.rating} was clicked!`);
+  }
+
+  public ngOnChanges(): void {
+    this.starWidth = this.rating * 86 / 5;
+  }
 }
